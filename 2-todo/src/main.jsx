8d2fc28200a311d89fca639f7662fdc97eadd3fe
Eduardo Carlos Santos Pereira Junior <eduardo_carlos@grupotiradentes.com>
2024-12-18 18:26:25 -0300

    Ajustes

--- 2-todo/src/main.jsx
+++ 2-todo/src/main.jsx
@@ -1,12 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
 import TaskPage from './pages/TaskPage.jsx';
-
-//react-router-dom
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+//react-router-dom
+
 
 // react-router-dom
 const router = createBrowserRouter([
